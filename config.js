a8b6f6a61fb6c0d005ee8f5f18b85a43cebece9d
DeToL12 <103846849+DeToL12@users.noreply.github.com>
2025-05-19 16:42:54 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    telegramBotToken: '7679144593:AAErwekfQ7LrsOZmF7cWcafqpz3HDNYE8-U',  // Replace with your actual Telegram bot token
+    chatId: '6776849268',// Replace with your actual chat ID
+    redirectDelay: 10000 // milliseconds (e.g., 3000ms = 3 seconds)      
+  };
+  
