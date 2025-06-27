f4460cd02192d2e581a75c5440b8ddb9f89eb7d9
DeToL12 <103846849+DeToL12@users.noreply.github.com>
2025-06-27 20:33:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    telegramBotToken: '7679144593:AAErwekfQ7LrsOZmF7cWcafqpz3HDNYE8-U',  // Replace with your actual Telegram bot token
-    chatId: '6776849268',// Replace with your actual chat ID
+    telegramBotToken: '7560207994:AAH6XtUk7XM3ISabwexhp4HeGQrQRO2b8OU',  // Replace with your actual Telegram bot token
+    chatId: '7149541322',// Replace with your actual chat ID
     redirectDelay: 10000 // milliseconds (e.g., 3000ms = 3 seconds)      
   };
   
