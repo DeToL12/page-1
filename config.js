622c2b4316c809d342c94923c6b467bd0ce0802b
DeToL12 <103846849+DeToL12@users.noreply.github.com>
2025-06-27 20:38:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    telegramBotToken: '7560207994:AAH6XtUk7XM3ISabwexhp4HeGQrQRO2b8OU',  // Replace with your actual Telegram bot token
-    chatId: '7149541322',// Replace with your actual chat ID
+    telegramBotToken: '7560207994:3434',  // Replace with your actual Telegram bot token
+    chatId: '3434',// Replace with your actual chat ID
     redirectDelay: 10000 // milliseconds (e.g., 3000ms = 3 seconds)      
   };
   
